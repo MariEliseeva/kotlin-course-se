4926b83d1dbe2fcabdca67c9ceab22a05fec79fe
Marieliseeva <eliseevamary@mail.ru>
2018-09-08 09:24:04 +0100

    Add hw1 with tests

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,62 @@
-package ru.hse.spb
+//package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+fun isInLanguage(sentence: String?): String {
+    val wordsArray = sentence?.split(" ") ?: return "NO"
+    if (wordsArray.size == 1) {
+        return isWord(wordsArray[0])
+    }
+    val isFeminine = isFeminine(wordsArray[0])
+    var index = 0
+    while (index < wordsArray.size && isAdjective(wordsArray[index])) {
+        if (isFeminine(wordsArray[index]) != isFeminine) {
+            return "NO"
+        }
+        index++
+    }
+    if (index == wordsArray.size
+        || isFeminine(wordsArray[index]) != isFeminine
+        || !isNoun(wordsArray[index])
+    ) {
+        return "NO"
+    }
+    index++
+    while (index < wordsArray.size && isVerb(wordsArray[index])) {
+        if (isFeminine(wordsArray[index]) != isFeminine) {
+            return "NO"
+        }
+        index++
+    }
+    if (index != wordsArray.size) {
+        return "NO"
+    }
+    return "YES"
+}
 
-    return words.joinToString(separator = " ")
+private fun isWord(word: String): String {
+    if (isAdjective(word) || isVerb(word) || isNoun(word)) {
+        return "YES"
+    }
+    return "NO"
+}
+
+private fun isAdjective(word: String): Boolean {
+    return word.endsWith("lios") || word.endsWith("liala")
+}
+
+private fun isVerb(word: String): Boolean {
+    return word.endsWith("initis") || word.endsWith("inites")
+}
+
+private fun isNoun(word: String): Boolean {
+    return word.endsWith("etr") || word.endsWith("etra")
+}
+
+private fun isFeminine(word: String): Boolean {
+    return word.endsWith("liala")
+            || word.endsWith("etra")
+            || word.endsWith("inites")
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    println(isInLanguage(readLine()))
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -5,7 +5,42 @@ import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testIsInLanguageOneWord() {
+        assertEquals("YES", isInLanguage("etr"))
+        assertEquals("YES", isInLanguage("feinites"))
+        assertEquals("YES", isInLanguage("taliala"))
+        assertEquals("NO", isInLanguage("wrongword"))
+    }
+
+    @Test
+    fun testIsInLanguageNoNoun() {
+        assertEquals("NO", isInLanguage("etis atis animatis etis atis amatis"))
+        assertEquals("NO", isInLanguage("nataliala kataliala"))
+        assertEquals("NO", isInLanguage("feinites inites"))
+    }
+
+    @Test
+    fun testIsInLanguageBadWord() {
+        assertEquals("NO", isInLanguage("nataliala kataliala vetra feinite"))
+        assertEquals("NO", isInLanguage("nataliala katalial vetra"))
+    }
+
+    @Test
+    fun testIsInLanguageBadGender() {
+        assertEquals("NO", isInLanguage("natalios kataliala vetra"))
+        assertEquals("NO", isInLanguage("nataliala vetr"))
+    }
+
+    @Test
+    fun testIsInLanguageManySentences() {
+        assertEquals("NO", isInLanguage("nataliala vetra feinites vetra"))
+        assertEquals("NO", isInLanguage("nataliala kataliala vetra feinites inites tes"))
+    }
+
+    @Test
+    fun testIsInLanguageOneCorrectSentence() {
+        assertEquals("YES", isInLanguage("nataliala kataliala vetra feinites"))
+        assertEquals("YES", isInLanguage("petr feinitis"))
+        assertEquals("YES", isInLanguage("nataliala kataliala vetra"))
     }
 }
